c6d48e7c6acd0af94ccab69c210963282f7a097a
Michal Sliwa <ms@nordicwindtechnology.com>
2024-04-15 14:28:38 +0200

    add RelayCommand

--- ModernDesignWPF/Core/ObservableObject.cs
+++ ModernDesignWPF/Core/ObservableObject.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModernDesignWPF.Core
 {
-    internal class ObservableObject
+    //class to notify subscribers (controllers) when a property value changes.
+
+    internal class ObservableObject : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //this refers to the current instance of ObjervableOjbect class
+        protected void OnPropertyChanged ([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (name));
+        }
     }
 }
